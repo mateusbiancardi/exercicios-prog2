e19f85c72f0403fb5256ced50458b26ac235da18
Mateus Biancardi <mateusbbdasilva@gmail.com>
2025-03-11 00:35:20 +0000

    sobe arquivo binario

---
+++ 09_arquivo_binario/Arq_Bin_02/Resultados/Mateus/completo/aluno.c
@@ -0,0 +1,38 @@
+#include "aluno.h"
+#include <stdlib.h>
+#include <string.h>
+
+struct Aluno {
+  char nome[100];
+  char dataNascimento[11];
+  char curso[50];
+  float coeficienteRendimento;
+  int periodo;
+};
+
+tAluno *CriarAluno(char *nome, char *dataNascimento, char *curso, int periodo,
+                   float coeficienteRendimento) {
+  tAluno *a = (tAluno *)calloc(1, sizeof(tAluno));
+
+  strcpy(a->nome, nome);
+  strcpy(a->dataNascimento, dataNascimento);
+  strcpy(a->curso, curso);
+  a->periodo = periodo;
+  a->coeficienteRendimento = coeficienteRendimento;
+
+  return a;
+};
+
+void DestruirAluno(tAluno *aluno) { free(aluno); };
+
+tAluno *LeAluno(FILE *arquivo_binario) {
+  tAluno *a = (tAluno *)calloc(1, sizeof(tAluno));
+  // int qtd;
+  // fread(&qtd, sizeof(int), 1, arquivo_binario);
+  fread(a, sizeof(tAluno), 1, arquivo_binario);
+  return a;
+};
+
+float GetCoeficienteRendimentoAluno(tAluno *aluno) {
+  return aluno->coeficienteRendimento;
+};
